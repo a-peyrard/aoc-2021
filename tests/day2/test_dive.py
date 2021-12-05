c94e5a5f4c24beacb65d65669f68fdcd8e6195de
Augustin Peyrard <augustin.peyrard@gmail.com>
2021-12-05 13:10:20 -0800

    feat(solution): adds part2 day2

--- tests/day2/test_dive.py
+++ tests/day2/test_dive.py
@@ -3,7 +3,7 @@ from typing import Tuple
 
 from hamcrest import assert_that, equal_to
 
-from aoc.day2.dive import Direction, calculate_position
+from aoc.day2.dive import Direction, calculate_position, calculate_position_with_aim
 from aoc.util.input import parse_input_file
 
 
@@ -50,3 +50,34 @@ forward 2""".splitlines(keepends=True)
 
         # THEN
         assert_that(res, equal_to(1840243))
+
+    def test_should_calculate_position_for_sample_part2(self):
+        # GIVEN
+        instructions = TestCalculatePosition._parse_input(
+            lines="""forward 5
+down 5
+forward 8
+up 3
+down 8
+forward 2""".splitlines(keepends=True)
+        )
+
+        # WHEN
+        res = calculate_position_with_aim(instructions)
+
+        # THEN
+        assert_that(res, equal_to(900))
+
+    def test_should_calculate_position_for_input_part2(self):
+        # GIVEN
+        instructions = parse_input_file(
+            origin=__file__,
+            filename='input.txt',
+            callback=TestCalculatePosition._parse_input
+        )
+
+        # WHEN
+        res = calculate_position_with_aim(instructions)
+
+        # THEN
+        assert_that(res, equal_to(1727785422))
